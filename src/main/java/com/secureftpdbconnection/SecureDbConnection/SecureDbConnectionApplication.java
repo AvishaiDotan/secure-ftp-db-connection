7267afcb073ab988ec2d8b3a6695003ba68d45ee
Avishai Dotan <108017307+AvishaiDotan@users.noreply.github.com>
2024-04-25 18:29:33 +0300

    change

---
+++ src/main/java/com/secureftpdbconnection/SecureDbConnection/SecureDbConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.secureftpdbconnection.SecureDbConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDbConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDbConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureftpdbconnection/SecureDbConnection/SecureDbConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureftpdbconnection.SecureDbConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDbConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
